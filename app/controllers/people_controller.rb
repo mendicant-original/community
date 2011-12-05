f4d0723782c76439a7f92feee0e7e893078acce0
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-05 15:31:33 -0500

    Add profile edit page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,4 +9,19 @@ class PeopleController < ApplicationController
     @person = UserDecorator.find_by_github(params[:id]).decorate
   end
 
+  def edit
+    @person = current_user
+  end
+
+  def update
+    @person = current_user
+
+    if @person.update_attributes(params[:user])
+      flash[:notice] = "Profile sucessfully updated"
+      redirect_to edit_person_path(@person.github)
+    else
+      render :action => :edit
+    end
+  end
+
 end
