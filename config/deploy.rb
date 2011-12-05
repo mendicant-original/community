da67aebb252f7a7c5dd686e4d11f589a0617a05b
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-05 16:48:19 -0500

    Add capistrano files

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'bundler/capistrano'
+
+set :application, "community"
+set :repository,  "git://github.com/rmu/community.git"
+
+set :scm, :git
+set :deploy_to, "/var/rapp/#{application}"
+
+set :user, "git"
+set :use_sudo, false
+
+set :deploy_via, :remote_cache
+
+set :branch, "master"
+
+# TODO Update to community.mendicantuniversity.org
+server "university.rubymendicant.com", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after 'deploy:update_code' do
+  {"database.yml"      => "config/database.yml",
+   "omniauth.rb"       => "config/initializers/omniauth.rb",
+   "secret_token.rb"   => "config/initializers/secret_token.rb",
+   "university_web.rb" => "config/initializers/university_web.rb"}.
+  each do |from, to|
+    run "ln -nfs #{shared_path}/#{from} #{release_path}/#{to}"
+  end
+end
+
+after "deploy", "deploy:migrate"
+after "deploy", 'deploy:cleanup'
+
+load 'deploy/assets'
