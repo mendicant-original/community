68e3e20759805451ef20f5d93a5735af4d64cf46
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-12 15:10:41 -0500

    Get those flash messages out of my way

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -3,8 +3,7 @@ var Flash = {};
 Flash.init = function(){
   setTimeout("$('div.flash').slideUp()", 2500);
 
-  $('div.flash a.hide').click(function(e){
-    $(this).parent('div.flash').slideUp();
-    e.preventDefault();
+  $('div.flash').mouseover(function(e){
+    $(this).slideUp();
   });
 }
