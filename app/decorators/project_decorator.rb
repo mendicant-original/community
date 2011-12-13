49b382fa144ad0195279eb51a6839856b1a1dbcd
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-13 09:42:19 -0500

    User credit on projects

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -30,8 +30,25 @@ class ProjectDecorator < ApplicationDecorator
     return project.name if project.description.blank?
 
     [ project.name,
-      h.content_tag(:span, h.strip_tags(project.description), :class => 'description')
+      h.content_tag(:span, :class => 'description') do
+        [h.strip_tags(project.description), from].compact.join(" ")
+      end
     ].join(' ').html_safe
+  end
+
+  def from
+    ["from", project.user.name].join(" ") unless project.core_project
+  end
+
+  def submitted_by
+    if project.core_project
+      [ h.link_to("Mendicant University", "http://university.rubymendicant.com"),
+        "Core Project"].join(" ")
+    else
+      ["Submitted by",
+        h.link_to(project.user.name, h.person_path(project.user.github))
+      ].join(" ")
+    end.html_safe
   end
 
   def source
