c3cafb263def0b5918d94247802465e3913b3c98
Jordan Byron <jordan@ducksoupsoftware.com>
2012-01-16 17:43:17 -0500

    For some reason whenever isn't picking up that we use bundler, so let's just force it to use bundle exec

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+# Make sure +bundle exec+ is used when executing rake tasks
+
+job_type :rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
 
 every 1.hour do
   rake "twitter:post"
