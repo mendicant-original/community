fab2fbdefdc386d30a3ec089ea17f2aa4e48e907
Jordan Byron <jordan@ducksoupsoftware.com>
2012-01-18 13:28:10 -0500

    Trying out some animation just for fun

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,9 +1,5 @@
 var Flash = {};
 
 Flash.init = function(){
-  setTimeout("$('div.flash').fadeOut()", 2500);
-
-  $('div.flash').mouseover(function(e){
-    $(this).fadeOut();
-  });
+  setTimeout("$('div.flash').removeClass('bounceIn').addClass('fadeOut')", 2500);
 }
