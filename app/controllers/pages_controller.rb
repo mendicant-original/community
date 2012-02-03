da64fbcd608ddab9d59da91ead47eec8dab35bbe
Jordan Byron <jordan.byron@gmail.com>
2012-02-03 16:20:48 -0500

    Fix pages controller so the tests are green

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,18 @@
 class PagesController < ApplicationController
   respond_to :html
+  before_filter :find_page
 
   def show
-    @page = PageDecorator.find_by_slug(params[:id])
-
-    if @page.nil?
-      raise ActionController::RoutingError.new('Not Found')
-    elsif @page.protected?
-      user_required
-    end
+    raise ActionController::RoutingError.new('Not Found') if @page.nil?
 
     respond_with(@page)
+  end
+
+  private
+
+  def find_page
+    @page = PageDecorator.find_by_slug(params[:id])
+
+    user_required if @page && @page.protected?
   end
 end
