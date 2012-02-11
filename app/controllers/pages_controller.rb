3296843f61724d9e3446c036b2c8fc8c2f7356f6
Jordan Byron <jordan.byron@gmail.com>
2012-02-11 10:17:02 -0500

    Fix double render on protected pages if user is not signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,13 @@
 class PagesController < ApplicationController
   respond_to :html
-  before_filter :find_page
 
   def show
-    raise ActionController::RoutingError.new('Not Found') if @page.nil?
-
-    respond_with(@page)
-  end
-
-  private
-
-  def find_page
     @page = PageDecorator.find_by_slug(params[:id])
 
-    user_required if @page && @page.protected?
+    raise ActionController::RoutingError.new('Not Found') if @page.nil?
+
+    return if @page && @page.protected? && user_required
+
+    respond_with(@page)
   end
 end
